267894b1b3616a4ed322bbeb2f4f75c5af8c7691
Maxim Pashkov <max.pashkov.rez@yandex.ru>
2020-06-17 22:12:23 +0400

    added MoneyPrinter test

---
+++ UnitTestsAdvanced/MoneyPrinter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Abstractions;
+
+namespace UnitTestsAdvanced
+{
+    class MoneyPrinter : MoneyPrinterInterface
+    {
+        readonly IFileSystem fileSystem;
+        readonly string path;
+
+        public MoneyPrinter(IFileSystem fileSystem, string path)
+        {
+            this.fileSystem = fileSystem;
+            this.path = path;
+        }
+
+        public MoneyPrinter(string path)
+        {
+            fileSystem = new FileSystem();
+            this.path = path;
+        }
+        public void print(string operation, string currency, int amount)
+        {
+            fileSystem.File.AppendAllLines(path, new string[] { "Operation: " + operation + "; Currency: " + currency + "; Amount: " + amount });
+        }
+    }
+}
---
+++ UnitTestsAdvanced/MoneyPrinterTest.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using System.IO.Abstractions.TestingHelpers;
+
+namespace UnitTestsAdvanced
+{
+    [TestFixture]
+    class MoneyPrinterTest
+    {
+        private MoneyPrinter moneyPrinter;
+        private MockFileSystem fileSystem;
+        private string filename;
+
+        public MoneyPrinterTest()
+        {
+            filename = "test.txt";
+        }
+
+        [SetUp] 
+        public void initMoneyPrinter()
+        {
+            fileSystem = new MockFileSystem();
+            moneyPrinter = new MoneyPrinter(fileSystem, filename);
+        }
+
+        [Test]
+        public void writeToFile()
+        {
+            moneyPrinter.print("1", "2", 3);
+
+            string[] lines = fileSystem.File.ReadAllLines(filename);
+
+            Assert.AreEqual(lines.Length, 1);
+
+            Assert.AreEqual(lines[0], "Operation: 1; Currency: 2; Amount: 3");
+        }
+
+        [Test]
+        public void writeMultilineToFile()
+        {
+            moneyPrinter.print("1", "2", 3);
+
+            moneyPrinter.print("3", "4", 5);
+
+            string[] lines = fileSystem.File.ReadAllLines(filename);
+
+            Assert.AreEqual(lines.Length, 2);
+
+            Assert.AreEqual(lines[0], "Operation: 1; Currency: 2; Amount: 3");
+
+            Assert.AreEqual(lines[1], "Operation: 3; Currency: 4; Amount: 5");
+        }
+    }
+}
